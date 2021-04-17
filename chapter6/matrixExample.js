81c0f208c4c8597330716474b80052e867ed8617
Farah Desktop <farahkrecinic@gmail.com>
2021-04-17 20:22:14 +0200

    Chap 6 examples done

--- chapter6/matrixExample.js
+++ chapter6/matrixExample.js
@@ -50,5 +50,31 @@ let matrix = new Matrix(2,2, (x,y) => `value ${x}x,${y}y`);
 matrix.set(0,0,'fish');
 matrix.set(1,1,'trees');
 for (let {x,y,value} of matrix){
-   // console.log(x,y,value);
+    //console.log(x,y,value);
+}
+
+class SymmetricMatrix extends Matrix {
+    constructor(size, element = (x,y) => undefined){
+        super(size, size, (x,y) => {
+            if (x < y) return element (y,x);
+            else return element (x,y);
+        });
+    }
+
+    set(x,y,value) {
+        super.set(x,y,value);
+        if (x!=y) { super.set(y,x,value)};
+    }
+}
+
+//let symMatrix = new SymmetricMatrix(5, (x,y) => `value ${x}x,${y}y`);
+let symMatrix = new SymmetricMatrix(5);
+symMatrix.set(0,0,'base');
+symMatrix.set(0,1,'fish');
+symMatrix.set(1,2,'hour');
+symMatrix.set(2,3,'why');
+symMatrix.set(3,4,'sad');
+symMatrix.set(4,4,'end');
+for (let {x,y,value} of symMatrix){
+    console.log(x,y,value);
 }
